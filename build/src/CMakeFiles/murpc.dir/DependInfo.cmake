d344a7ddfe0e45925632776c263d093177390437
Majunjie11 <2028427172@qq.com>
2020-02-22 23:23:10 +0800

    murpc

---
+++ build/src/CMakeFiles/murpc.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ma/pro/murpc/src/loadxmlconfig.cpp" "/home/ma/pro/murpc/build/src/CMakeFiles/murpc.dir/loadxmlconfig.cpp.o"
+  "/home/ma/pro/murpc/src/rpc_meta.pb.cc" "/home/ma/pro/murpc/build/src/CMakeFiles/murpc.dir/rpc_meta.pb.cc.o"
+  "/home/ma/pro/murpc/src/rpcclient.cpp" "/home/ma/pro/murpc/build/src/CMakeFiles/murpc.dir/rpcclient.cpp.o"
+  "/home/ma/pro/murpc/src/rpcserver.cpp" "/home/ma/pro/murpc/build/src/CMakeFiles/murpc.dir/rpcserver.cpp.o"
+  "/home/ma/pro/murpc/src/zookeeperutils.cpp" "/home/ma/pro/murpc/build/src/CMakeFiles/murpc.dir/zookeeperutils.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ma/pro/murpc/build/thirdparty/tinyxml/CMakeFiles/tinyxml.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../thirdparty/tinyxml"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
